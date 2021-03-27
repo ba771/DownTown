aec75f1a40c0ffd6b5d36313bd49081ee2b830f6
Kamaldeen Samaila <Kamal@Kamals-MacBook.local>
2021-03-27 15:28:05 +0100

    Created product-item component, added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    id?: number;
+    store_id?: number;
+    title: string;
+    price: string;
+    quantity: number;
+    image_url?: string;
+    rating?: number;
+    description?: string;
+}

3c3edf2f0842e3771da7575be57cee73bbdcf7d1
Baraatu Audu <ummitrh@Baraatus-MacBook-Air.local>
2021-04-04 22:25:37 +0400

    login, signup, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import{ RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
@@ -8,7 +7,20 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component'
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import{ MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule} from '@angular/material/list';
+import { MatButtonModule } from  '@angular/material/button';
+
+
+
 
 
 
@@ -19,17 +31,27 @@ import { SignupComponent } from './components/signup/signup.component'
     NavbarComponent,
     ProductItemComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    SidenavComponent
   ],
   imports: [
+    MatListModule,
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
     RouterModule.forRoot([
       { path:'login', component:LoginComponent},
       { path:'signup', component:SignupComponent},
       { path:'hub', component:ProductItemComponent}
-    ])
+    ]),
 
     
 
